66f5d2cf91caab432e97b1c6416a3693bef75f36
milttreonna <mowens@basecampcodingacademy.org>
2017-03-16 16:43:09 -0500

    made cost variables

---
+++ instrurent.js
@@ -0,0 +1,11 @@
+var aguitar_cost= 439.99;
+var banjo_cost= 62.27;
+var clarinet_cost=152.36;
+var conga_cost=35.10;
+var drum_cost=118.30;
+var eguitar_cost=899.99;
+var piano_cost=221.13;
+var sax_cost=410.02;
+var trumpet_cost=317.07;
+var violin_cost=91.00;
+
