d36c79815d3383765c65391f9e34bed628b2a252
milttreonna <mowens@basecampcodingacademy.org>
2017-03-16 21:34:27 -0500

    changed weeks variables/ fixed banjo function and aguitar function

--- instrurent.js
+++ instrurent.js
@@ -20,11 +20,15 @@ var clarinetTotal = function() {
 }
 
 var aguitarTotal =function() {
-
+ var weeks_value= document.getElementById("aguitarWeeks").value;
+    var full_total=weeks_value*aguitar_cost;
+    document.getElementById("aguitarTotal").innerHTML=full_total;
 }
 
 var banjoTotal = function() {
- 
+ var weeks_value= document.getElementById("banjoWeeks").value;
+    var full_total=weeks_value*banjo_cost;
+    document.getElementById("banjoTotal").innerHTML=full_total;
 }
 
 var congaTotal = function() {
