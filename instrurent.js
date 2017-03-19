9fc36fa2c334d2e1f623ffe49960e6031b12f941
milttreonna <mowens@basecampcodingacademy.org>
2017-03-18 20:04:45 -0500

    fixed violinTotal()

--- instrurent.js
+++ instrurent.js
@@ -84,6 +84,7 @@ var trumpetTotal= function(){
 var violinTotal= function(){
   var weeks_value= document.getElementById("violinWeeks").value;
     var full_total=weeks_value*violin_cost;
+    full_total=full_total.toFixed(2);
     document.getElementById("violinTotal").innerHTML="$"+full_total;
  
 }
