f412a2dcc44737826f22dd5f3fa0728dccec3e4a
milttreonna <mowens@basecampcodingacademy.org>
2017-03-18 19:41:05 -0500

    fixing aguitarTotal()

--- instrurent.js
+++ instrurent.js
@@ -22,6 +22,7 @@ var clarinetTotal = function() {
 var aguitarTotal =function() {
  var weeks_value= document.getElementById("aguitarWeeks").value;
     var full_total=weeks_value*aguitar_cost;
+    full_total=full_total.toFixed(2);
     document.getElementById("aguitarTotal").innerHTML="$"+full_total;
 }
 
