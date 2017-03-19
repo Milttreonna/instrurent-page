4c3e97308be3dd8a4191504e0e123a352c451c62
milttreonna <mowens@basecampcodingacademy.org>
2017-03-18 20:35:51 -0500

    fixed eguitar and aguitar rent price

--- instrurent.js
+++ instrurent.js
@@ -1,9 +1,9 @@
-var aguitar_cost= 439.99;
+var aguitar_cost= 57.29;
 var banjo_cost= 62.27;
 var clarinet_cost=152.36;
 var conga_cost=35.10;
 var drum_cost=118.30;
-var eguitar_cost=899.99;
+var eguitar_cost=117.00;
 var piano_cost=221.13;
 var sax_cost=410.02;
 var trumpet_cost=317.07;
