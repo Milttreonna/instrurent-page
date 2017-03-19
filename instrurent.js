161ba842bb19a61b5b8865221c4488e17e941a8d
milttreonna <mowens@basecampcodingacademy.org>
2017-03-18 19:35:07 -0500

    added  to full totals

--- instrurent.js
+++ instrurent.js
@@ -22,61 +22,61 @@ var clarinetTotal = function() {
 var aguitarTotal =function() {
  var weeks_value= document.getElementById("aguitarWeeks").value;
     var full_total=weeks_value*aguitar_cost;
-    document.getElementById("aguitarTotal").innerHTML=full_total;
+    document.getElementById("aguitarTotal").innerHTML="$"+full_total;
 }
 
 var banjoTotal = function() {
  var weeks_value= document.getElementById("banjoWeeks").value;
     var full_total=weeks_value*banjo_cost;
-    document.getElementById("banjoTotal").innerHTML=full_total;
+    document.getElementById("banjoTotal").innerHTML="$"+full_total;
 }
 
 var congaTotal = function() {
     var weeks_value= document.getElementById("congaWeeks").value;
     var full_total=weeks_value*conga_cost;
-    document.getElementById("congaTotal").innerHTML=full_total;
+    document.getElementById("congaTotal").innerHTML="$"+full_total;
  
 }
 
 var drumTotal= function() {
     var weeks_value= document.getElementById("drumWeeks").value;
     var full_total=weeks_value*drum_cost;
-    document.getElementById("drumTotal").innerHTML=full_total;
+    document.getElementById("drumTotal").innerHTML="$"+full_total;
  
 }
 
 var eguitarTotal= function(){
   var weeks_value= document.getElementById("eguitarWeeks").value;
     var full_total=weeks_value*eguitar_cost;
-    document.getElementById("eguitarTotal").innerHTML=full_total;
+    document.getElementById("eguitarTotal").innerHTML="$"+full_total;
  
 }
 
 var pianoTotal= function(){
   var weeks_value= document.getElementById("pianoWeeks").value;
     var full_total=weeks_value*piano_cost;
-    document.getElementById("pianoTotal").innerHTML=full_total;
+    document.getElementById("pianoTotal").innerHTML="$"+full_total;
  
 }
 
 var saxTotal= function() {
   var weeks_value= document.getElementById("saxWeeks").value;
     var full_total=weeks_value*sax_cost;
-    document.getElementById("saxTotal").innerHTML=full_total;
+    document.getElementById("saxTotal").innerHTML="$"+full_total;
  
 }
 
 var trumpetTotal= function(){
     var weeks_value= document.getElementById("trumpetWeeks").value;
     var full_total=weeks_value*trumpet_cost;
-    document.getElementById("trumpetTotal").innerHTML=full_total;
+    document.getElementById("trumpetTotal").innerHTML="$"+full_total;
  
 }
 
 var violinTotal= function(){
   var weeks_value= document.getElementById("violinWeeks").value;
     var full_total=weeks_value*violin_cost;
-    document.getElementById("violinTotal").innerHTML=full_total;
+    document.getElementById("violinTotal").innerHTML="$"+full_total;
  
 }
 
