06d0f00d02518facce2c76c6d8baa537ae4c4cb9
milttreonna <mowens@basecampcodingacademy.org>
2017-03-18 20:04:05 -0500

    fixed trumpetTotal()

--- instrurent.js
+++ instrurent.js
@@ -76,6 +76,7 @@ var saxTotal= function() {
 var trumpetTotal= function(){
     var weeks_value= document.getElementById("trumpetWeeks").value;
     var full_total=weeks_value*trumpet_cost;
+    full_total=full_total.toFixed(2);
     document.getElementById("trumpetTotal").innerHTML="$"+full_total;
  
 }
